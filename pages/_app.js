d4be6955bcb1c971d7e4202f8eb03c0c1e23fdf8
Zenia Gist <zeniagist@gmail.com>
2021-09-06 08:31:28 -0500

    added styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "../styles/global.scss";
+import "../styles/navbar.scss";
+import "../styles/index.scss";
+import "../styles/contact.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
