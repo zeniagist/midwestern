dabcfaea834d2accfcc759e082421548468c6a91
Zenia Gist <zeniagist@gmail.com>
2021-09-09 18:53:54 -0500

    added firestore store contact data

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -6,22 +6,20 @@ import Navbar from "../components/navbar";
 import SimpleLayout from "../components/simplelayout";
 import ContactForm from "../components/contactform";
 import styles from "../styles/contact.module.scss";
+import { db } from "../util/firebase";
 
 export default function Contact() {
   const [contactInfo, setContactInfo] = useState([]);
   const [contactForm, setContactForm] = useState([]);
 
-  // get JSON contact info content
   useEffect(() => {
-    let url = "https://api.mwi.dev/content/contact";
-
-    // contact content
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setContactInfo(data.data[0]);
-        setContactForm(data.data[0].page);
-      });
+    // get static data from firestor cont
+    db.collection("contactpage").onSnapshot((snapshot) => {
+      const postData = [];
+      snapshot.forEach((doc) => postData.push({ ...doc.data(), id: doc.id }));
+      setContactInfo(postData[0]);
+      setContactForm(postData[1]);
+    });
   }, []);
 
   // get URL pathname
@@ -33,16 +31,18 @@ export default function Contact() {
         {/* Contact Info */}
         <div className={styles.contactInfoContainer}>
           <div className={styles.infoContainer}>
-            <h1 className="headingOneTitle">{contactInfo.title}</h1>
+            <h1 className="headingOneTitle">{contactInfo.contactInfoTitle}</h1>
             <div className="headingOneTitleUnderline"></div>
-            <p className={styles.contactInfoContent}>{contactInfo.content}</p>
+            <p className={styles.contactInfoContent}>
+              {contactInfo.contactInfoContent}
+            </p>
           </div>
         </div>
 
         {/* Contact Form */}
         <div className={styles.contactFormContainer}>
           <div className={styles.formContainer}>
-            <h2 className={styles.formTitle}>{contactForm.name}</h2>
+            <h2 className={styles.formTitle}>{contactForm.contactFormTitle}</h2>
             <ContactForm />
           </div>
         </div>
